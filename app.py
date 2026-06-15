873a6273cc180492fd748aea05df34edb339c29e
Praveen2029-singh <praveenrana1675@gmail.com>
2026-06-15 18:51:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load model
-with open('/house_price_model.pkl', 'rb') as f:
+with open('house_price_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 st.set_page_config(page_title="House Price Prediction", page_icon="🏠")
